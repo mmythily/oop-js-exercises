53dd6cb80ff135e460268fbb3933ce327d223ea6
mmythily <mythily.mudunuru@gmail.com>
2019-06-27 07:55:19 -0400

    add method to superclass

--- p2-inheritance.js
+++ p2-inheritance.js
@@ -9,21 +9,32 @@ class Person {
     bio() {
         return `My name is ${this.name} and here's my quirky fact: ${this.quirkyFact}`;
     }
+
+    currentWeek(week) {
+        return `Current week is ${week}`;
+    }
 }
 class Student extends Person {
     // stays in Student class since it's specific to students only
     enroll(cohort) {
         this.cohort = cohort;
     }
-  }
-  class Mentor extends Person {
+}
+class Mentor extends Person {
     // specific to mentors
     goOnShift() {
         this.onShift = true;
     }
-  
     // specific to mentors
     goOffShift() {
         this.onShift = false;
     }
-  }
+}
+
+const michelle = new Person('michelle','love trees');
+
+// const michelle = new Student();;
+// michelle.cohort('may25');
+
+console.log(michelle, michelle.bio(), michelle.currentWeek(5));
+
