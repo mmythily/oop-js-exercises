0022887644e62202820d881cbc2dcb1dcb83e55b
mmythily <mythily.mudunuru@gmail.com>
2019-06-26 22:48:30 -0400

    p2 before inheritance

---
+++ p2-inheritance.js
@@ -0,0 +1,40 @@
+class Student {
+    // this constructor is identical to that of a mentor!
+    constructor(name, quirkyFact) {
+      this.name = name;
+      this.quirkyFact = quirkyFact;
+    }
+  
+    // here is a method that is specific to students
+    enroll(cohort) {
+      this.cohort = cohort;
+    }
+  
+    // identical! Smells of code duplication
+    bio() {
+      return `My name is ${this.name} and here's my quirky fact: ${this.quirkyFact}`;
+    }
+  }
+  
+  class Mentor {
+    // this constructor is identical to that of a student!
+    constructor(name, quirkyFact) {
+      this.name = name;
+      this.quirkyFact = quirkyFact;
+    }
+  
+    // specific to mentors
+    goOnShift() {
+      this.onShift = true;
+    }
+  
+    // specific to mentors
+    goOffShift() {
+      this.onShift = false;
+    }
+  
+    // identical! Smells of code duplication
+    bio() {
+      return `My name is ${this.name} and here's my quirky fact: ${this.quirkyFact}`;
+    }
+  }
