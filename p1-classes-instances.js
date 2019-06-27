97f450d4701747d60613f0d2549d8b337affc9e6
mmythily <mythily.mudunuru@gmail.com>
2019-06-26 22:19:02 -0400

    construct pizza with toppings,size,crust

--- p1-classes-instances.js
+++ p1-classes-instances.js
@@ -1,17 +1,19 @@
 class Pizza {           // has two methods-base and add Topping and 1 property - toppings
-    constructor() {
+    constructor(size, crust) {
+        this.size = size;
+        this.crust = crust;
         this.toppings = ['cheese']
     }
     addTopping(topping) {
         this.toppings.push(topping)
     }
 }
-let pizza1 = new Pizza();
+let pizza1 = new Pizza('small','deep dish');
 pizza1.addTopping('mushrooms')
 pizza1.addTopping('jalapeno')
 pizza1.addTopping('sun dried tomatoes')
 
-let pizza2 = new Pizza();
+let pizza2 = new Pizza('large','thin');
 pizza2.addTopping('mushrooms')
 pizza2.addTopping('jalapeno')
 
